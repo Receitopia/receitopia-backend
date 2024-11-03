9af4494ec1042e9d450e4a74550b3a579c5d2c8b
Isabella Sofia Martins <106943011+ibellmartins@users.noreply.github.com>
2024-11-03 17:18:49 -0300

    delete receitopia/receitopia directory

--- receitopia/receitopia/src/main/java/com/example/receitopia/repository/CardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.receitopia.repository;
-
-import com.example.receitopia.entities.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long>{
-    
-}
